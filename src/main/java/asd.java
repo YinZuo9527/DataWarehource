9025ea4af6d3aaf0d4806a82224b2fa64f9c873b
YinZuo <YinZuo@163.com>
2020-09-16 10:23:36 +0800

    mastar commit Yinzuo9527

--- src/main/java/asd.java
+++ src/main/java/asd.java
@@ -3,6 +3,7 @@ public class asd {
      public static void main(String[]args){
          System.out.println("沙雕");
          System.out.println("二比");
+         System.out.println("贱人");
      }
 
 }
